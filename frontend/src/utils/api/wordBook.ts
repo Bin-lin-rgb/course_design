cb502f6270edfdf632fb5dc8d13f8887fe2c80fb
Bin-lin-rgb <3036140937@qq.com>
2023-06-13 20:24:22 +0800

    fix

--- frontend/src/utils/api/wordBook.ts
+++ frontend/src/utils/api/wordBook.ts
@@ -2,7 +2,8 @@ import request from "./request";
 
 export function GetWordList1() {
   return request({
-    url: `http://127.0.0.1:8080/basic-api/word/getList1`,
+    url: `api/basic-api/word/getList1`,
+    // url: `http://127.0.0.1:8080/basic-api/word/getList1`,
     method: "get"
   });
 }
